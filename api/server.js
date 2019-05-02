41e3ffe96a8bcfc7f6a2bad0bff806f869837809
jmtblei <47536941+jmtblei@users.noreply.github.com>
2019-05-02 13:38:46 -0700

    initial, templates

---
+++ api/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const server = express();
+server.use(helmet());
+server.use(express.json());
+
+server.get("/", (req, res) => {
+    res.send("It's working!");
+});
+
+module.exports = server;
